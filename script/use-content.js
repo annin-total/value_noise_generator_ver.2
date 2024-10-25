ed454aad77a0bd3870ecbecd3d7ef2ca1196d5ce
annin-total <130845842+annin-total@users.noreply.github.com>
2024-10-25 10:34:42 +0900

    Update use-content.js

--- script/use-content.js
+++ script/use-content.js
@@ -3,12 +3,12 @@ const howtouseTable = document.querySelector(".howtouse-table");
 const rows = document.querySelectorAll(".howtouse-tr");
 const howtouseImages = [
   "./img/img9.jpg",
+  "./img/img7.jpg",
   "./img/img2.jpg",
-  "./img/img3.jpg",
   "./img/img4.jpg",
   "./img/img5.jpg",
   "./img/img6.jpg",
-  "./img/img7.jpg",
+  "./img/img10.jpg",
   "./img/img9.jpg",
 ];
 
